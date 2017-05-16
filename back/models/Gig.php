91eacd1dda54293d18b458c0d382fe2a35b18733
Clément Roig <clm.roig@gmail.com>
2017-05-17 01:42:05 +0200

    Models until Member.php (included) done

---
+++ back/models/Gig.php
@@ -0,0 +1,128 @@
+<?php
+include('config/connexionBD.php');
+
+class Gig {
+
+    // ========= ATTRIBUTES ========= //
+    private $gigId;         // integer
+    private $gigPrice;      // float (numeric(6,2) in DB)
+    private $gigPlace;      // text
+    private $gigStyleId;    // integer
+    // ============================= //
+
+
+    // ==== Misc requests ==== //
+
+    public function getIdMax() {
+        $maxId = myPDO()->query('SELECT MAX(gig_id) FROM gigs');
+        return $maxId->fetch()[0];
+    }
+
+    public function getGigs() {
+        $req = myPDO()->prepare('SELECT * FROM gigs');
+        $req->execute();
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Gig");
+        return json_encode($object);
+    }
+
+    public function countGigs() {
+        $req = myPDO()->prepare('SELECT gig_id FROM gigs');
+        $req->execute();
+        $count = $req->rowCount();
+        return $count;
+    }
+    // ======================= //
+
+    // ==== GET requests ==== //
+    public function getGig($gigId) {
+        $req = myPDO()->prepare('SELECT * FROM gigs WHERE gig_id = :gig_id');
+        $req->execute(array(':gig_id' => $gigId));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Gig");
+        return json_encode($object);
+    }
+
+    public function getGigsByPrice($gigPrice) {
+        $req = myPDO()->prepare('SELECT * FROM gigs WHERE gig_price = :gig_price');
+        $req->execute(array(':gig_price' => $gigPrice));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Gig");
+        return json_encode($object);
+    }
+
+    public function getGigsByPlace($gigPlace) {
+        $req = myPDO()->prepare('SELECT * FROM gigs WHERE gig_place = :gig_place');
+        $req->execute(array(':gig_place' => $gigPlace));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Gig");
+        return json_encode($object);
+    }
+
+    public function getGigsByStyleId($gigStyleId) {
+        $req = myPDO()->prepare('SELECT * FROM gigs WHERE gig_style_id = :gig_style_id');
+        $req->execute(array(':gig_style_id' => $gigStyleId));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Gig");
+        return json_encode($object);
+    }
+
+    // ====================== //
+
+
+    // ==== POST / PUT / DELETE requests ==== //
+
+    public function insertGig($gigPrice, $gigPlace, $gigStyleId) {
+        $gigId = $this->getIdMax() + 1;
+        $sql = "INSERT INTO gigs VALUES (:gig_id, :gig_price, :gig_place, :gig_style_id)";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':gig_id' => $gigId,
+          ':gig_price' => $gigPrice,
+          ':gig_place' => $gigPlace,
+          ':gig_style_id' => $gigStyleId
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function updateGig($gigId, $gigPrice, $gigPlace, $gigStyleId) {
+        $sql = myPdo()->prepare("UPDATE gigs SET gig_name=:gig_name, gig_formed_in=:gig_formed_in , gig_style_id=:gig_style_id WHERE gig_id = :gig_id");
+        $params = [
+          ':gig_name' => $gigName,
+          ':gig_formed_in' => $gigFormedIn,
+          ':gig_style_id' => $gigStyleId,
+          ':gig_id' => $gigId
+        ];
+        try {
+            $sql->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function deleteGig($gigId) {
+        $sql = "DELETE FROM gigs WHERE gig_id = :gig_id";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':gig_id' => $gigId,
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+    // ====================================== //
+
+}
---
+++ back/models/Member.php
@@ -0,0 +1,127 @@
+<?php
+include('config/connexionBD.php');
+
+class Member {
+
+    // ========= ATTRIBUTES ========= //
+    private $memberId;          // integer
+    private $memberFirstName;   // text
+    private $memberLastName;    // text
+    private $memberPseudo;      // text
+    // ============================= //
+
+
+    // ==== Misc requests ==== //
+
+    public function getIdMax() {
+        $maxId = myPDO()->query('SELECT MAX(member_id) FROM members');
+        return $maxId->fetch()[0];
+    }
+
+    public function getMembers() {
+        $req = myPDO()->prepare('SELECT * FROM members');
+        $req->execute();
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Member");
+        return json_encode($object);
+    }
+
+    public function countMembers() {
+        $req = myPDO()->prepare('SELECT member_id FROM members');
+        $req->execute();
+        $count = $req->rowCount();
+        return $count;
+    }
+    // ======================= //
+
+    // ==== GET requests ==== //
+    public function getMember($memberId) {
+        $req = myPDO()->prepare('SELECT * FROM members WHERE member_id = :member_id');
+        $req->execute(array(':member_id' => $memberId));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Member");
+        return json_encode($object);
+    }
+
+    public function getMembersByFirstName($memberFirstName) {
+        $req = myPDO()->prepare('SELECT * FROM members WHERE member_first_name = :member_first_name');
+        $req->execute(array(':member_first_name' => $memberFirstName));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Member");
+        return json_encode($object);
+    }
+
+    public function getMembersByLastName($memberLastName) {
+        $req = myPDO()->prepare('SELECT * FROM members WHERE member_last_name = :member_last_name');
+        $req->execute(array(':member_last_name' => $memberLastName));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Member");
+        return json_encode($object);
+    }
+
+    public function getMembersByPseudo($memberPseudo) {
+        $req = myPDO()->prepare('SELECT * FROM members WHERE member_pseudo = :member_pseudo');
+        $req->execute(array(':member_pseudo' => $memberPseudo));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Member");
+        return json_encode($object);
+    }
+    // ====================== //
+
+
+    // ==== POST / PUT / DELETE requests ==== //
+
+    public function insertMember($memberFirstName, $memberLastName, $memberPseudo) {
+        $memberId = $this->getIdMax() + 1;
+        $sql = "INSERT INTO members VALUES (:member_id, :member_first_name, :member_last_name, :member_pseudo)";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':member_id' => $memberId,
+          ':member_first_name' => $memberFirstName,
+          ':member_last_name' => $memberLastName,
+          ':member_pseudo' => $memberPseudo
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function updateMember($memberId, $memberFirstName, $memberLastName, $memberPseudo) {
+        $sql = myPdo()->prepare("UPDATE members SET member_first_name=:member_first_name, member_last_name=:member_last_name , member_pseudo=:member_pseudo WHERE member_id = :member_id");
+        $params = [
+          ':member_first_name' => $memberFirstName,
+          ':member_last_name' => $memberLastName,
+          ':member_pseudo' => $memberPseudo,
+          ':member_id' => $memberId
+        ];
+        try {
+            $sql->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function deleteMember($memberId) {
+        $sql = "DELETE FROM members WHERE member_id = :member_id";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':member_id' => $memberId,
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+    // ====================================== //
+
+}
