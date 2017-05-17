28c3db5859d2e35ef3239f912ec69b6dbb795430
Clément Roig <clm.roig@gmail.com>
2017-05-17 11:05:19 +0200

    [Models] done

---
+++ back/models/Song.php
@@ -0,0 +1,127 @@
+<?php
+include('config/connexionBD.php');
+
+class Song {
+
+    // ========= ATTRIBUTES ========= //
+    private $songId;            // integer
+    private $songName;          // text
+    private $songTrackNumber;   // integer
+    private $songLength;        // integer
+    // ============================= //
+
+
+    // ==== Misc requests ==== //
+
+    public function getIdMax() {
+        $maxId = myPDO()->query('SELECT MAX(song_id) FROM songs');
+        return $maxId->fetch()[0];
+    }
+
+    public function getAllSongs() {
+        $req = myPDO()->prepare('SELECT * FROM songs');
+        $req->execute();
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Song");
+        return json_encode($object);
+    }
+
+    public function countSongs() {
+        $req = myPDO()->prepare('SELECT song_id FROM songs');
+        $req->execute();
+        $count = $req->rowCount();
+        return $count;
+    }
+    // ======================= //
+
+    // ==== GET requests ==== //
+    public function getSong($songId) {
+        $req = myPDO()->prepare('SELECT * FROM songs WHERE song_id = :song_id');
+        $req->execute(array(':song_id' => $songId));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Song");
+        return json_encode($object);
+    }
+
+    public function getSongsByName($songName) {
+        $req = myPDO()->prepare('SELECT * FROM songs WHERE song_name = :song_name');
+        $req->execute(array(':song_name' => $songName));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Song");
+        return json_encode($object);
+    }
+
+    public function getSongsByTrackNumber($songTrackNumber) {
+        $req = myPDO()->prepare('SELECT * FROM songs WHERE song_track_number = :song_track_number');
+        $req->execute(array(':song_track_number' => $songTrackNumber));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Song");
+        return json_encode($object);
+    }
+
+    public function getSongsByLength($songLength) {
+        $req = myPDO()->prepare('SELECT * FROM songs WHERE song_length = :song_length');
+        $req->execute(array(':song_length' => $songLength));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Song");
+        return json_encode($object);
+    }
+    // ====================== //
+
+
+    // ==== POST / PUT / DELETE requests ==== //
+
+    public function insertSong($songName, $songTrackNumber, $songLength) {
+        $songId = $this->getIdMax() + 1;
+        $sql = "INSERT INTO songs VALUES (:song_id, :song_name, :song_track_number, :song_length)";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':song_id' => $songId,
+          ':song_name' => $songName,
+          ':song_track_number' => $songTrackNumber,
+          ':song_length' => $songLength
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function updateSong($songId, $songName, $songTrackNumber, $songLength) {
+        $sql = myPdo()->prepare("UPDATE songs SET song_name=:song_name, song_track_number=:song_track_number , song_length=:song_length WHERE song_id = :song_id");
+        $params = [
+          ':song_name' => $songName,
+          ':song_track_number' => $songName,
+          ':song_length' => $songTrackNumber,
+          ':song_id' => $songId
+        ];
+        try {
+            $sql->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function deleteSong($songId) {
+        $sql = "DELETE FROM songs WHERE song_id = :song_id";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':song_id' => $songId,
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+    // ====================================== //
+
+}
--- back/models/Style.php
+++ back/models/Style.php
@@ -1,15 +1,22 @@
 <?php
-// Classe d'accès à la BD
 include('config/connexionBD.php');
 
 class Style {
 
     // ========= ATTRIBUTES ========= //
-    private $styleId;        //integer
-    private $styleName;      // text
+    private $styleId;            // integer
+    private $styleName;          // text
     // ============================= //
 
-    public function getStyles() {
+
+    // ==== Misc requests ==== //
+
+    public function getIdMax() {
+        $maxId = myPDO()->query('SELECT MAX(style_id) FROM styles');
+        return $maxId->fetch()[0];
+    }
+
+    public function getAllStyles() {
         $req = myPDO()->prepare('SELECT * FROM styles');
         $req->execute();
         $object = $req->fetchAll(PDO::FETCH_CLASS, "Style");
@@ -22,11 +29,79 @@ class Style {
         $count = $req->rowCount();
         return $count;
     }
+    // ======================= //
 
-    public function getStyle($id) {
-        $req = myPDO()->prepare('SELECT * FROM styles WHERE style_id = ?');
-        $req->execute(array($id));
+    // ==== GET requests ==== //
+    public function getStyle($styleId) {
+        $req = myPDO()->prepare('SELECT * FROM styles WHERE style_id = :style_id');
+        $req->execute(array(':style_id' => $styleId));
         $object = $req->fetchAll(PDO::FETCH_CLASS, "Style");
         return json_encode($object);
     }
+
+    public function getStylesByName($styleName) {
+        $req = myPDO()->prepare('SELECT * FROM styles WHERE style_name = :style_name');
+        $req->execute(array(':style_name' => $styleName));
+        $object = $req->fetchAll(PDO::FETCH_CLASS, "Style");
+        return json_encode($object);
+    }
+    // ====================== //
+
+
+    // ==== POST / PUT / DELETE requests ==== //
+
+    public function insertStyle($styleName) {
+        $styleId = $this->getIdMax() + 1;
+        $sql = "INSERT INTO styles VALUES (:style_id, :style_name)";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':style_id' => $styleId,
+          ':style_name' => $styleNames
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function updateStyle($styleId, $styleName) {
+        $sql = myPdo()->prepare("UPDATE styles SET style_name=:style_name WHERE style_id = :style_id");
+        $params = [
+          ':style_name' => $styleName,
+          ':style_id' => $styleId
+        ];
+        try {
+            $sql->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+
+    public function deleteStyle($styleId) {
+        $sql = "DELETE FROM styles WHERE style_id = :style_id";
+        $req = myPdo()->prepare($sql);
+        $params = [
+          ':style_id' => $styleId,
+        ];
+        try {
+            $req->execute($params);
+            return true;
+        }
+        catch (Exception $e) {
+            echo 'Error request "'.$sql.'" : ';
+            var_dump($e->getMessage());
+            return false;
+        }
+    }
+    // ====================================== //
+
 }
