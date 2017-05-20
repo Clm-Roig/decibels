8d9fcff3e439770feab17a384701836c03fd7969
Clément Roig <clm.roig@gmail.com>
2017-05-20 23:50:39 +0200

    no cache

--- front/js/app.js
+++ front/js/app.js
@@ -4,8 +4,9 @@ angular.module('Decibels',[
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
 }])
-
+/*
 .config(['$httpProvider', function ($httpProvider) {
         // enable http caching
        $httpProvider.defaults.cache = true;
 }])
+*/
