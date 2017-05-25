3211d7756b835c159fb298e3635ada07737ece6e
Clément Roig <clm.roig@gmail.com>
2017-05-25 18:06:22 +0200

    ini_set off

--- back/Routeur.php
+++ back/Routeur.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-
-    // TODO : API... 
+    // TODO : API...
     if(!empty($_GET)) {
         $request = $_GET;
     }
--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
-    ini_set('display_errors', 1);
     echo file_get_contents('front/components/base/base.html');
